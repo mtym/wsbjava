bfe931ca5d4df62b9b1472f4ab79642acbe0126d
mtym <maciektym1@gmail.com>
2020-05-04 14:02:12 +0200

    zad 0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Animal {
+    String species;
+    String name;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("Dog");
+        dog.name = "Tunia";
+
+        System.out.println("Hi, I'm " + dog.name);
+
+        dog.feed();
+
+        Human me = new Human();
+        me.firstName = "Maciej";
+        me.lastName = "Nowak";
+        me.pet = dog;
+
+        me.pet.feed();
+        System.out.println(me.pet.species);
+
+        me.pet = new Animal("Cat");
+        me.pet.name = "Myszojeleń";
+
+        me.pet.feed();
+
+        System.out.println(me.pet.species);
+    }
+}
